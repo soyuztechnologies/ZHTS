b03316cb67b0786b9d4cc3f2e77d9d57ed85c788
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2021-01-25 12:05:32 +0530

    Nav back

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -41,14 +41,15 @@ sap.ui.define([
 		},
 		
 		onNavBack: function() {
-			var oHistory = History.getInstance();
-			var sPreviousHash = oHistory.getPreviousHash();
-			if (sPreviousHash !== undefined) {
-				window.history.go(-1);
-			} else {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("overview", true);
-			}
+			// var oHistory = History.getInstance();
+			// var sPreviousHash = oHistory.getPreviousHash();
+			// if (sPreviousHash !== undefined) {
+			// 	window.history.go(-1);
+			// } else {
+			// 	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			// 	oRouter.navTo("overview", true);
+			// }
+			this.getOwnerComponent().getRouter().navTo("Main");
 		}
 	});
 
